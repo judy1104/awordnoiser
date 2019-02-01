722e32cdc5850665ab079c1b394e81b393945073
jyyoo@rsupport.com <jyyoo@rsupport.com>
2019-02-01 15:03:56 +0900

    noise 클래스 구조 설계

--- Awordnoiser/noise.cpp
+++ Awordnoiser/noise.cpp
@@ -1,2 +1,72 @@
 #include "stdafx.h"
 #include "noise.h"
+
+CNoise::CNoise(CString strWord, int nFilter /*= 0*/, BOOL bOption1 /*= TRUE*/, BOOL bOption2 /*= TRUE*/, BOOL bOption3 /*= TRUE*/, BOOL bOption4 /*= TRUE*/, BOOL bOption5 /*= TRUE*/)
+{
+	m_strWord = strWord;
+	m_nfilter = nFilter; 
+	m_bOption1 = bOption1;
+	m_bOption2 = bOption2;
+	m_bOption3 = bOption3;
+	m_bOption4 = bOption4;
+	m_bOption5 = bOption5;
+}
+
+CNoise::~CNoise()
+{
+
+}
+
+BOOL CNoise::GetWordList(CStringList strList)
+{
+	BOOL bResult = TRUE; 
+
+	strList.RemoveAll();
+
+	if (m_bOption1 == TRUE)
+	{
+		ChangeSpelling(strList);
+	}
+	
+	if ((m_bOption2 | m_bOption3 | m_bOption4 | m_bOption5) == TRUE)
+	{
+		ChangeSpelling(strList);
+	}
+
+	return bResult; 
+}
+
+BOOL CNoise::ChangeSpelling(CStringList& strList)
+{
+
+}
+
+BOOL CNoise::AddSpecialChar(CStringList& strList)
+{
+
+}
+
+BOOL CNoise::GetSpecialChar1(CStringList& strList)
+{
+
+}
+
+BOOL CNoise::GetSpecialChar2(CStringList& strList)
+{
+
+}
+
+BOOL CNoise::GetSpecialChar3(CStringList& strList)
+{
+
+}
+
+BOOL CNoise::GetSpecialChar4(CStringList& strList)
+{
+
+}
+
+BOOL CNoise::GetSpecialChar5(CStringList& strList)
+{
+
+}
--- Awordnoiser/noise.h
+++ Awordnoiser/noise.h
@@ -1,1 +1,29 @@
 #pragma once
+
+
+class CNoise
+{
+public:
+	CNoise(CString strWord, int nFilter = 0, BOOL bOption1 = TRUE, BOOL bOption2 = TRUE, BOOL bOption3 = TRUE, BOOL bOption4 = TRUE, BOOL bOption5 = TRUE);
+	~CNoise();
+	BOOL GetWordList(CStringList strList);
+
+private:	
+	BOOL ChangeSpelling(CStringList& strList);
+	BOOL AddSpecialChar(CStringList& strList);
+
+	BOOL GetSpecialChar1(CStringList& strList);
+	BOOL GetSpecialChar2(CStringList& strList);
+	BOOL GetSpecialChar3(CStringList& strList);
+	BOOL GetSpecialChar4(CStringList& strList);
+	BOOL GetSpecialChar5(CStringList& strList);
+
+private:
+	CString		m_strWord = _T("");
+	int			m_nfilter = 0;
+	BOOL		m_bOption1 = FALSE;
+	BOOL		m_bOption2 = FALSE;
+	BOOL		m_bOption3 = FALSE;
+	BOOL		m_bOption4 = FALSE;
+	BOOL		m_bOption5 = FALSE;	
+};
